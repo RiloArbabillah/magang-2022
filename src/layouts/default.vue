24d2ec29bb0f6ced825ef118c0c2c8713382f415
RiloArbabillah <r16615011@gmail.com>
2022-07-11 11:40:14 +0800

    add portofolio default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,7 +7,7 @@
 	<main
 		class="
 			p-8
-			dark:bg-gray-800 dark:text-white
+			dark:bg-[#162641] dark:text-white
 			bg-white
 			text-gray-800
 			min-h-screen
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 	import { useStore } from '@/store';
 	import VitailseLogo from '@/assets/logo.png';
+	import UserImage from '@/assets/users.png';
 	useHead({
-		title: 'Vitailse | Opinionated vite starter template',
+		title: 'Magang Vue 2022',
 	});
 
 	const store = useStore();
@@ -10,42 +11,61 @@
 </script>
 
 <template>
-	<div class="grid place-items-center min-h-[80vh]">
+	<div class="grid place-items-center min-h-[80vh] py-10">
 		<div>
-			<img :src="VitailseLogo" width="300" class="mx-auto" />
-			<p class="lg:text-left text-center">
-				{{ t('intro.desc') }}
-				<a
-					href="https://tailwindcss.com/"
-					class="text-blue-500 hover:underline"
-					>TailwindCSS</a
-				>
-			</p>
-			<p class="text-center">
-				{{ t('intro.github') }}
-				<a
-					class="text-blue-500 hover:underline"
-					href="https://github.com/zynth17/vitailse"
-					>Github Repo</a
-				>
-			</p>
-			<div class="mt-5 text-center">
-				<button
-					@click="store.$state.count++"
-					class="
-						px-4
-						py-2
-						dark:bg-blue-800
-						bg-blue-500
-						text-white
-						rounded
-					"
-				>
-					Count : {{ store.$state.count }}
-				</button>
+			<div class="grid max-w-lg text-center place-items-center">
+				<img :src="UserImage" width="300" class="mx-auto" />
+				<h1 class="uppercase font-bold text-xl mt-10">Profile</h1>
+				<div class="text-sm text-justify w-1/3">
+					<p>
+						Nama : Rilo Arbabillah
+					</p>
+					<p>
+						TTL : Samarinda, 2x-xx-xx
+					</p>
+					<p>
+						Agama: Islam
+					</p>
+					<p>
+						Jenis Kelamin : L
+					</p>
+				</div>
+			</div>
+			<div class="max-w-lg text-center mt-10">
+				<h1 class="uppercase font-bold text-xl">About</h1>
+				<p class="text-sm text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, impedit ipsum alias unde accusamus ut dignissimos a incidunt nemo nam. Labore, dignissimos. Culpa iste doloremque accusantium unde, tenetur magni nesciunt!</p>
+			</div>
+			<div class="grid max-w-lg text-center place-items-center mt-10">
+				<h1 class="uppercase font-bold text-xl">Contact</h1>
+				<div class="text-sm text-justify w-1/3">
+					<p>
+						Lorem
+					</p>
+					<p>
+						Ipsum
+					</p>
+					<p>
+						Dolor
+					</p>
+					<p>
+						Sit Amet
+					</p>
+				</div>
+			</div>
+			<div class="max-w-lg text-center mt-10">
+				<h1 class="uppercase font-bold text-xl">Hobby</h1>
+				<p class="text-sm text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, impedit ipsum alias unde accusamus ut dignissimos a incidunt nemo nam. Labore, dignissimos. Culpa iste doloremque accusantium unde, tenetur magni nesciunt!</p>
+			</div>
+			<div class="max-w-lg text-center mt-10">
+				<h1 class="uppercase font-bold text-xl">Skill</h1>
+				<p class="text-sm text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, impedit ipsum alias unde accusamus ut dignissimos a incidunt nemo nam. Labore, dignissimos. Culpa iste doloremque accusantium unde, tenetur magni nesciunt!</p>
+			</div>
+			<div class="max-w-lg text-center mt-10">
+				<h1 class="uppercase font-bold text-xl">Education</h1>
+				<p class="text-sm text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, impedit ipsum alias unde accusamus ut dignissimos a incidunt nemo nam. Labore, dignissimos. Culpa iste doloremque accusantium unde, tenetur magni nesciunt!</p>
 			</div>
 		</div>
-		<router-link
+		<!-- <router-link
 			:to="{ name: 'other-page' }"
 			class="
 				mt-5
@@ -55,7 +75,7 @@
 				hover:underline
 			"
 			>{{ t('pages.other.menu') }}</router-link
-		>
+		> -->
 	</div>
 </template>
 
